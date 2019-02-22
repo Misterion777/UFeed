6deac93efe2bd1a10e59bc547f1540eb6c4ed353
Ilya Lasy <ilyalas@gmail.com>
2019-02-23 00:46:15 +0300

    pod configuration added

--- UFeed/SupportFiles/AppDelegate.swift
+++ UFeed/SupportFiles/AppDelegate.swift
@@ -1,4 +1,4 @@
-//
+	//
 //  AppDelegate.swift
 //  UFeed
 //
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
@@ -42,5 +44,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+    
 }
 
