8c2502ae8f6fe9954ebafbc959ada165782b011b
Admin <admin@Mac-Admin.local>
2019-04-01 13:02:31 +0300

    add collection view, but it still ugly

---
+++ UFeed/Controller/SignInCollectionViewController.swift
@@ -0,0 +1,111 @@
+//
+//  SignInCollectionViewController.swift
+//  UFeed
+//
+//  Created by Admin on 31/03/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "cellId"
+
+
+
+class SignInCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout  {
+    
+    
+    var networkImgs = [#imageLiteral(resourceName: "71172"), #imageLiteral(resourceName: "71172"), #imageLiteral(resourceName: "71172"), #imageLiteral(resourceName: "71172")]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView!.register(SignInCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSeg8ue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return networkImgs.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! SignInCollectionViewCell
+        let currentLastItem = networkImgs[indexPath.row]
+        cell.image = currentLastItem
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+    
+}
+
+
+class SignInCollectionViewFlowLayout : UICollectionViewFlowLayout{
+    
+    
+    override func prepare() {
+        super.prepare()
+        
+        let minColumnWidth = CGFloat(50.0)
+        let cellHeight = CGFloat(70)
+        
+        guard let collectionView = collectionView else { return }
+        
+        let availableWidth = collectionView.bounds.inset(by: collectionView.layoutMargins).width
+        let maxNumColumns = Int(availableWidth / minColumnWidth)
+        let cellWidth = (availableWidth / CGFloat(maxNumColumns)).rounded(.down)
+        
+        self.itemSize = CGSize(width: cellWidth, height: cellHeight)
+        self.sectionInset = UIEdgeInsets(top: self.minimumInteritemSpacing, left: 0.0, bottom: 0.0, right: 0.0)
+        self.sectionInsetReference = .fromSafeArea
+    }
+    
+    
+}
