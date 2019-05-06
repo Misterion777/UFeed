d7f4ae67029a534f259d1a943fa38caf6354ecdf
Ilya Lasy <Ilyalas@gmail.com>
2019-05-06 10:32:38 +0300

    instagram enhance

---
+++ UFeed/Model/Utility/ErrorsParser.swift
@@ -0,0 +1,9 @@
+//
+//  ErrorsParser.swift
+//  UFeed
+//
+//  Created by Ilya on 5/6/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import Foundation
---
+++ UFeed/View/SettingsView/MarkAllHeaderView.swift
@@ -0,0 +1,9 @@
+//
+//  MarkAllHeaderView.swift
+//  UFeed
+//
+//  Created by Ilya on 5/5/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import Foundation
